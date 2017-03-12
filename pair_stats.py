542a7b18891bc994ed69dca07f2895e422dbda3a
yangjici <jiyang@ucdavis.edu>
2017-03-11 18:59:29 -0800

    Update pair_stats.py

--- pair_stats.py
+++ pair_stats.py
@@ -35,10 +35,11 @@ CDF= 1-(15/20) = 0.25
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pylab import plot,show,hist
+from pylab import plot,show,hist,title
 from scipy.stats.kde import gaussian_kde
+import scipy.stats as scs
 
-df = pd.read_csv("~/Downloads/data/admissions.csv")
+df = pd.read_csv("/Users/twilightidol/Downloads/data (1)/admissions.csv")
 
 
 def my_cov(dat):
@@ -83,9 +84,83 @@ def income_code(dat):
         val = "high income"
     return val
 
-df["income_group"] = df.apply(income_code,axis=1)
+df_new=df
+
+df_new["income_group"] = df_new.apply(income_code,axis=1)
 
 
 
-df.groupby("income_group")["gpa"].plot(kind="kde")
-plt.legend(["low income", "medium income", "high income"], loc='upper left')
+df_new.groupby("income_group")["gpa"].plot(kind="kde")
+plt.legend(["high income", "low income", "medium income"], loc='upper left')
+
+show()
+
+
+nintey_percentile =df_new.groupby("income_group")["gpa"].agg(lambda x: np.percentile(x,90))
+
+
+'''
+PART 3
+'''
+
+#1
+
+df2 = pd.read_csv("/Users/twilightidol/Downloads/data (1)/admissions_with_study_hrs_and_sports.csv")
+
+x=df2["hrs_studied"]
+
+y=df2["gpa"]
+
+slope, intercept, r_value, p_value, std_err = scs.linregress(x, y)
+
+line = slope*x+intercept
+
+plot(x,y,'o', x, line)
+
+title("Linear fit of Hours studied Versus GPA")
+
+show()
+
+
+"""
+3: pearson correlation is great at detecting purely first degree linear relationship
+while spearman correlation is good at detecting monotonic relationship in general,
+since the relationship is curvilinear, spearman is more sensitive to the correlation
+than pearson.
+"""
+
+'''
+PART 4
+'''
+
+def profit_range(n):
+    profits=[]
+    for _ in range(n):
+        views=np.random.randint(5000,6000)
+        conversions=scs.binom.rvs(views,0.12)
+        whole_sale_prop= scs.binom.rvs(conversions,0.2)/conversions
+        profits.append(conversions*(whole_sale_prop*50+(1-whole_sale_prop)*60))
+    return (np.percentile(profits,2.5),np.percentile(profits,97.5))
+
+'''
+given 10000 simulations, 95 percentile of profit falls in the range of
+34980 and 44341z
+
+'''
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print my_cov(df.ix[:,:3])
+    print normalize(df.ix[:,:3])
+    print df.head()
+    print df_new.head()
+    print nintey_percentile
+    print scs.pearsonr(x,y)
+    print scs.spearmanr(x,y)
